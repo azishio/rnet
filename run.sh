199a84b94c145d210e08784c2236501645adce91
azishio <azishio3141@gmail.com>
2024-10-06 21:16:34 +0900

    ✨ 実行スクリプト

--- run.sh
+++ run.sh
@@ -4,13 +4,10 @@ set -eu
 # download mokuroku.csv
 curl -L https://cyberjapandata.gsi.go.jp/xyz/experimental_rvrcl/mokuroku.csv.gz | gzip -d > mokuroku.csv
 
-# build docker image
-docker build -t rnet ghcr.io/azishio/rnet:latest
-
 # run docker container
 
-docker run rnet collect "$@"
-docker run rnet delaunay -i ./river_node.csv
+docker run --rm --name rnet -v "${PWD}:/data" -it ghcr.io/azishio/rnet:main collect "$@"
+docker run --rm --name rnet -v "${PWD}:/data" -it ghcr.io/azishio/rnet:main delaunay -i ./river_node.csv
 
 
 
